d0db3db723557dbaff550d3c755951d69528e0d7
Jonatan <Snchez>
2021-04-01 13:50:13 +0200

    Añadidas las rutas, y muestra los errores

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const debug = require("debug")("mi-app:principal");
 const express = require("express");
 const chalk = require("chalk");
 const { program } = require("commander");
-const { reset } = require("chalk");
 const morgan = require("morgan");
 
 program.option("-p, --puerto <puerto>", "Puerto para el servidor");
@@ -28,3 +27,43 @@ server.on("error", err => {
 app.use(morgan("dev"));
 app.use(express.static("public"));
 
+
+app.get("/metro/lineas", (req, res, next) => {
+
+  res.json({
+    id: "Venga, hasta luego.",
+    linea: "L2",
+    despricion: "Descripción de la linea"
+  });
+});
+app.get("/metro/linea/:linea?", (req, res, next) => {
+  res.json({
+    linea: req.params.linea,
+    descripcion: "Descripción de la línea",
+    paradas: [
+      {
+        id: "x",
+        nombre: "Nombre parada"
+      }
+    ]
+  });
+});
+
+app.put((req, res, next) => {
+  res.status(404).json({ error: true, mensaje: "Te pensabas que podías hackearme" });
+});
+
+app.post((req, res, next) => {
+  res.status(404).json({ error: true, mensaje: "Te pensabas que podías hackearme" });
+});
+app.delete((req, res, next) => {
+  res.status(404).json({ error: true, mensaje: "Te pensabas que podías hackearme" });
+});
+app.use((req, res, next) => {
+  res.status(404).json({ error: true, mensaje: "Recurso no encontrado" });
+});
+
+app.use((err, req, res, next) => {
+  debug(err);
+  res.status(500).send({ error: true, mensaje: "Ha petado algo" });
+});
