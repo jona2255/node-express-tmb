dd6aa138a7ba0b1800bd656942463c1af7d4b0f9
Jonatan <Snchez>
2021-04-01 13:24:04 +0200

    Añadida funcionalidad de assar el num de puerto por parámetro

--- index.js
+++ index.js
@@ -6,4 +6,25 @@ const { program } = require("commander");
 const { reset } = require("chalk");
 const morgan = require("morgan");
 
+program.option("-p, --puerto <puerto>", "Puerto para el servidor");
+program.parse(process.argv);
+const options = program.opts();
+
 const app = express();
+
+const puerto = options.puerto || process.env.PUERTO || 5000;
+
+const server = app.listen(puerto, () => {
+  debug(chalk.yellow(`Servidor escuchando en el puerto ${chalk.green(puerto)}.`));
+});
+
+server.on("error", err => {
+  debug(chalk.red("Ha ocurrido un error al levantar el servidor"));
+  if (err.code === "EADDRINUSE") {
+    debug(chalk.red(`El puerto ${puerto} está ocupado`));
+  }
+});
+
+app.use(morgan("dev"));
+app.use(express.static("public"));
+
