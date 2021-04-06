c7bbf08c367c991c3a655ab492b9d3b992169e9c
Jonatan <Snchez>
2021-04-06 11:37:45 +0200

    Cambiada la forma en la que trabajan los errores

--- index.js
+++ index.js
@@ -43,20 +43,22 @@ app.get("/metro/lineas", (req, res, next) => {
 });
 app.get("/metro/linea/:linea?", (req, res, next) => {
   if (!req.params.linea) {
-    res.status(404).json({ error: true, mensaje: "Recurso no encontrado" });
+    const error = new Error("No se ha buscado ninguna línea");
+    error.codigo = 403;
+    throw error;
   }
   fetch(`${process.env.url_api_tmb}?app_id=${process.env.api_id}&app_key=${process.env.api_key}`)
     .then(resp => resp.json())
     .then(lineas => {
       if (!lineas.features.find(linea => linea.properties.NOM_LINIA === req.params.linea)) {
-        res.status(404).json({ error: true, mensaje: "Recurso no encontrado" });
+        res.status(404).json({ error: true, mensaje: "La línea solicitada no existe" });
       } else {
-        const codiLinia = lineas.features.filter(linea => linea.properties.NOM_LINIA === req.params.linea);
+        const codiLinia = lineas.features.filter(linea => linea.properties.NOM_LINIA.toLowerCase() === req.params.linea.toLowerCase());
         fetch(`${process.env.url_api_tmb}/${codiLinia[0].properties.CODI_LINIA}/estacions?app_id=${process.env.api_id}&app_key=${process.env.api_key}`)
           .then(resp => resp.json())
           .then(paradas => {
             res.json({
-              linea: req.params.linea,
+              linea: req.params.linea.toUpperCase(),
               descripcion: paradas.features[0].properties.DESC_SERVEI,
               paradas: paradas.features.map(parada => ({
                 id: parada.properties.ID_ESTACIO,
@@ -80,11 +82,13 @@ app.post("/*", (req, res, next) => {
 app.delete("/*", (req, res, next) => {
   res.status(403).json(mensajeError);
 });
-app.get("/*", (req, res, next) => {
+app.use((req, res, next) => {
   res.status(404).json({ error: true, mensaje: "Recurso no encontrado" });
 });
 
 app.use((err, req, res, next) => {
   debug(err);
-  res.status(500).send({ error: true, mensaje: "Ha petado algo" });
+  const codigoError = err.codigo || 500;
+  const codigoMensaje = err.message || "Ha ocurrido un error general";
+  res.status(codigoError).send({ error: true, mensaje: codigoMensaje });
 });
